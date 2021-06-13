426bc647e4e77ed651d22f4b8f65082b4f01bc3a
Sebas Contreras <eseceve@gmail.com>
2021-06-12 21:55:57 -0400

    chore(CI): install deps in CI job

--- .releaserc.js
+++ .releaserc.js
@@ -3,8 +3,8 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
     "@semantic-release/changelog",
+    "@semantic-release/github",
     "@semantic-release/npm",
     "@semantic-release/git",
   ],
