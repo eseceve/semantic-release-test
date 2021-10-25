928aa8e3981232bb01cfbfdc1739b15f7a2f9c66
Sebas Contreras <eseceve@gmail.com>
2021-10-25 14:50:42 -0300

    fix(App): bla bla

--- .releaserc.js
+++ .releaserc.js
@@ -1,9 +1,18 @@
 module.exports = {
   branches: [
     { name: "main" },
-    { name: "v+([0-9])", channel: "${name.replace(/v/, '')}", range: "${name.replace(/v/, '')}.x" },
+    {
+      name: "v+([0-9])",
+      channel: "${name.replace(/v/, '')}",
+      range: "${name.replace(/v/, '')}.x",
+    },
     { name: "release", channel: "rc", prerelease: "rc" },
-    { name: "release-v+([0-9])", prerelease: "${name.replace(/release/, 'rc')}", type: "maintenance" },
+    {
+      name: "release-v+([0-9])",
+      channel: "${name.replace(/release-/, '')}rc",
+      prerelease: "${name.replace(/release-/, '')}rc",
+      type: "maintenance",
+    },
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
